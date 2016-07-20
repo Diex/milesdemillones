1308e405dffe6670ffc5c9968efcba533c5f4a83
Diex <aboiledtiger@gmail.com>
2016-07-20 13:20:31 -0300

    anda el display con spi

--- display_02/display_02.ino
+++ display_02/display_02.ino
@@ -1,10 +1,10 @@
 #include <limits.h>
 
-#define SEG_DATA   12   //PORTB4 (pin 12)
-#define SEG_SH     11     //PORTB1 (pin 9)
-#define LATCH      10 // PORTB2 (pin 10)
-#define DIG_SH     9          //PORTB0 (pin 8)
-#define DIG_DATA   8       //PORTB3 (pin 11)
+#define SEG_DATA   12   
+#define SEG_SH     13   
+#define LATCH      10 
+#define DIG_SH     9 
+#define DIG_DATA   6 
 
 #define DIG_0 B10000000
 #define DIG_1 B01000000
@@ -15,7 +15,7 @@
 #define DIG_6 B00000010
 #define DIG_7 B00000001
 
-uint8_t digits[] = {DIG_0, DIG_1 , DIG_2 , DIG_3 , DIG_4 , DIG_5 , DIG_6 , DIG_7};
+short digits[] = {DIG_0, DIG_1 , DIG_2 , DIG_3 , DIG_4 , DIG_5 , DIG_6 , DIG_7};
 
 #define NUM_DIGITS 8
 // como es una pantalla cada bit es un pixel (blanco o negro)
@@ -26,15 +26,15 @@ void setup() {
   // TODO test para chequear que este bien conectado.
   pinMode(LATCH, OUTPUT);
   pinMode(SEG_DATA, OUTPUT);
-  pinMode(SEG_DATA, OUTPUT);
-  pinMode(DIG_SH, OUTPUT);
+  pinMode(SEG_SH, OUTPUT);
+  pinMode(DIG_DATA, OUTPUT);
   pinMode(DIG_SH, OUTPUT);
 
 
   Serial.begin(115200);
   
   latch();  
-
+  setRands();
 }
 
 
@@ -44,32 +44,46 @@ long ptime = 0;
 
 void loop() {
   render(count);
-  count++;
+//  count++;
 }
 
 
+short rands[8];
+
+void setRands(){
+  for(int i =0 ; i < 8 ; i++){
+    rands[i] = random(255);
+  }
+}
 // escanea linea por linea y la dibuja...
 void render(int count) {
-  int number = 0;
+//  int number = 0;
   for (int x = 0 ; x < NUM_DIGITS; x++) {
-    shiftOut(SEG_DATA, SEG_SH, LSBFIRST, random(0, 255));   
+    shiftOut(SEG_DATA, SEG_SH, LSBFIRST, rands[x]);   
     shiftOut(DIG_DATA, DIG_SH, LSBFIRST, digits[x]);
     latch();
+    delay(5);
   }
 }
 
 
 short segments[] = { 
-  B01000000,  //A
-  B00100000,  //B
-  B00010000,  //C
-  B00000100,  //D
-  B00000010 //E
-   
+  B00000000,  //A
+  B01000000,  //B
+  B00100000,  //C
+  B00010000,  //D
+  B00001000,  //E
+  B00000100,  //F
+  B00000010   //G   
 };
+
+short zero(){
+ return  segments[0] | segments[1] | segments[2] | segments[3] | segments[4] | segments[5] | segments[6];
+}
+
 
 void latch() {
   digitalWrite(LATCH, LOW);
   digitalWrite(LATCH, HIGH);
-  digitalWrite(LATCH, LOW);
+//    digitalWrite(LATCH, LOW);
 }
---
+++ display_03_spi/display_03_spi.ino
@@ -0,0 +1,237 @@
+#include <limits.h>
+#include <SPI.h>
+
+//#define SEG_DATA   12   
+//#define SEG_SH     13   
+#define LATCH      6
+#define DIG_SH     8 
+#define DIG_DATA   7 
+
+#define DIG_0 B10000000
+#define DIG_1 B01000000
+#define DIG_2 B00100000
+#define DIG_3 B00010000
+#define DIG_4 B00001000
+#define DIG_5 B00000100
+#define DIG_6 B00000010
+#define DIG_7 B00000001
+
+#define A 0
+#define B 1
+#define C 2
+#define D 3
+#define E 4
+#define F 5
+#define G 6
+#define N 7
+
+short digits[] = {DIG_0, DIG_1 , DIG_2 , DIG_3 , DIG_4 , DIG_5 , DIG_6 , DIG_7};
+
+#define NUM_DIGITS 8
+// como es una pantalla cada bit es un pixel (blanco o negro)
+// solo se que mide tantos x tantos pixels
+
+uint8_t data[8];
+
+void setup() {
+  // TODO test para chequear que este bien conectado.
+  pinMode(LATCH, OUTPUT);
+  pinMode(DIG_DATA, OUTPUT);
+  pinMode(DIG_SH, OUTPUT);
+
+  SPI.begin();
+  SPI.setBitOrder(LSBFIRST);
+  SPI.setDataMode(SPI_MODE0);
+  SPI.setClockDivider (SPI_CLOCK_DIV8);
+
+
+  Serial.begin(115200);
+  
+  latch();  
+//  setRands();
+
+}
+
+
+
+long count = 0;
+long ptime = 0;
+
+void loop() {
+  render();
+
+}
+
+
+uint8_t rands[8];
+
+void setRands(){
+  for(int i =0 ; i < 8 ; i++){
+    rands[i] = zero(); //random(255);
+  }
+}
+
+void render() {
+  for (int x = 0 ; x < NUM_DIGITS; x++) {
+digitalWrite(LATCH, LOW);
+    uint8_t digit = selectDigit(x + 8);
+    SPI.transfer(digit);
+    shiftOut(DIG_DATA, DIG_SH, LSBFIRST, digits[x]);
+//    latch();
+    digitalWrite(LATCH, HIGH);
+    delayMicroseconds(200);
+  }
+}
+
+
+uint8_t selectDigit(int data){
+  switch(data){
+  
+    case 0:
+    return zero();
+    
+    case 1:
+    return one();
+    
+    case 2:
+    return two();
+    
+    case 3:
+    return three();
+    
+    case 4:
+    return four();
+    
+    case 5:
+    return five();
+    
+    case 6:
+    return six();
+    
+    case 7:
+    return seven();
+    
+    case 8:
+    return eigth();
+    
+    case 9:
+    return nine();
+    
+    case 10:
+    return a();
+    
+    case 11:
+    return b();
+    
+    case 12:
+    return c();
+    
+    case 13:
+    return d();
+    
+    case 14:
+    return e();
+    
+    case 15:
+    return f();
+    
+  }
+
+}
+
+
+short segments[] = { 
+  B10000000,  //A
+  B01000000,  //B
+  B00100000,  //C
+  B00010000,  //D
+  B00001000,  //E
+  B00000100,  //F
+  B00000010   //G   
+};
+
+
+uint8_t zero(){
+ return  segments[A] | segments[B] | segments[C] | segments[D] | segments[E] | segments[F];
+}
+
+uint8_t one(){
+ return  segments[B] | segments[C];
+}
+
+uint8_t two(){
+ return  segments[A] | segments[B] | segments[G] | segments[E] | segments[D];
+}
+
+uint8_t three(){
+ return  segments[A] | segments[B] | segments[G] | segments[C] | segments[D];
+}
+
+uint8_t four(){
+ return  segments[F] | segments[G] | segments[B] | segments[C] ;
+}
+
+uint8_t five(){
+ return  segments[A] | segments[F] | segments[G] | segments[C] | segments[D];
+}
+
+uint8_t six(){
+ return  segments[A] | segments[F] | segments[G] | segments[E] | segments[D] | segments[C];
+}
+
+uint8_t seven(){
+ return  segments[A] | segments[B] | segments[C];
+}
+
+uint8_t eigth(){
+ return  segments[A] | segments[B] | segments[C] | segments[D] | segments[E] | segments[F] | segments[G];
+}
+
+uint8_t nine(){
+ return  segments[A] | segments[F] | segments[G] | segments[B] | segments[C] ;
+}
+
+uint8_t a(){
+ return  segments[A] | segments[B] | segments[C] | segments[E] | segments[F] | segments[G];
+}
+
+uint8_t b(){
+ return   segments[C] | segments[D] | segments[E] | segments[F] | segments[G];
+}
+
+uint8_t c(){
+ return  segments[A] | segments[D] | segments[E] | segments[F] ;
+}
+
+uint8_t d(){
+ return  segments[B] | segments[C] | segments[D] | segments[E] | segments[G];
+}
+
+uint8_t e(){
+ return  segments[A] | segments[D] | segments[E] | segments[F] | segments[G];
+}
+
+uint8_t f(){
+ return  segments[A] | segments[E] | segments[F] | segments[G];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void latch() {
+  digitalWrite(LATCH, LOW);
+  digitalWrite(LATCH, HIGH);
+//    digitalWrite(LATCH, LOW);
+}
